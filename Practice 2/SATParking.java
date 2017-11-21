b6e50b9d0666b13d24f144dbdf1211ff43699de6
fernando2393 <garciasanzfernando2393@gmail.com>
2017-11-21 20:16:50 +0100

    Parser done and matrix creation

--- Practice 2/SATParking.java
+++ Practice 2/SATParking.java
@@ -8,14 +8,30 @@ public static void main(String[] args) {
 
         String filename = args[0];
         String line = null;
+        int lane_number;
+        int locations;
+        int j = 0;
 
         try {
                 FileReader filereader = new FileReader (filename);
                 BufferedReader bufferedreader = new BufferedReader (filereader);
 
-                while((line = bufferedreader.readLine()) != null) {
-                        System.out.println(line);
+                line = bufferedreader.readLine();
+                String data[] = line.split(" ");
+                lane_number = Integer.parseInt(data[0]);
+                locations = Integer.parseInt(data[1]);
+
+                String parking[][] = new String [lane_number][locations];
+
+                while((line = bufferedreader.readLine()) != null){
+                  String car_info[] = line.split(" ");
+                  for (int i = 0; i < locations; i++) {
+                    parking[j][i] = car_info[i];
+                  }
+                  j++;
+                  System.out.println(line);
                 }
+
                 bufferedreader.close();
         }
 
