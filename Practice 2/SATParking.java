4c6687c58605385ea37bd0cdf936e5e42e7b0bd5
fernando2393 <garciasanzfernando2393@gmail.com>
2017-11-24 10:53:32 +0100

    Parser improved

--- Practice 2/SATParking.java
+++ Practice 2/SATParking.java
@@ -2,6 +2,16 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
+// import org.jacop.core.BooleanVar;
+// import org.jacop.core.Store;
+// import org.jacop.jasat.utils.structures.IntVec;
+// import org.jacop.satwrapper.SatWrapper;
+// import org.jacop.search.DepthFirstSearch;
+// import org.jacop.search.IndomainMin;
+// import org.jacop.search.Search;
+// import org.jacop.search.SelectChoicePoint;
+// import org.jacop.search.SimpleSelect;
+// import org.jacop.search.SmallestDomain;
 
 public class SATParking {
 public static void main(String[] args) {
@@ -32,11 +42,35 @@ public static void main(String[] args) {
                         System.out.println(line);
                 }
 
+                String categories[][] = new String [lane_number][locations];
+                for(int i = 0; i < lane_number; i++){
+                  System.out.println(" ");
+                  for(int k = 0; k < locations; k++){
+                    categories[i][k] = String.valueOf(parking[i][k].charAt(0));
+                    System.out.print(categories[i][k]+" ");
+                  }
+                }
+                System.out.println("");
+                String arrival[][] = new String [lane_number][locations];
+                for(int i = 0; i < lane_number; i++){
+                  System.out.println(" ");
+                  for(int k = 0; k < locations; k++){
+                    arrival[i][k] = String.valueOf(parking[i][k].charAt(1));
+                    System.out.print(arrival[i][k]+" ");
+                  }
+                }
+
                 bufferedreader.close();
         }
 
         catch(IOException ex) {
                 System.out.println("Error reading file '" + filename + "'");
         }
+
+        // Store store = new Store();
+        // BooleanVar A = new BooleanVar(store, "Car of type A in tile");
+    		// BooleanVar B = new BooleanVar(store, "Car of type B in tile");
+    		// BooleanVar C = new BooleanVar(store, "Car of type C in tile");
+
 }
 }
