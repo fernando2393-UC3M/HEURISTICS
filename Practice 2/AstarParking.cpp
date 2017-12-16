ac96d0193d9de10aebe849ab4f197a11711aca3a
fernando2393 <garciasanzfernando2393@gmail.com>
2017-12-16 18:36:15 +0100

    Changes

--- Practice 2/AstarParking.cpp
+++ Practice 2/AstarParking.cpp
@@ -7,38 +7,77 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 
-        vector<string> cars;
+        vector<string> initialcars;
         string aux;
 
-        //This part gets all strings from the input file
-        ifstream file(argv[1]);
-        while (!file.eof()) {
-                file >> aux;
-                cars.push_back(aux);
+        //This part gets all strings from the initial state file
+        ifstream initialfile(argv[1]);
+        while (!initialfile.eof()) {
+                initialfile >> aux;
+                initialcars.push_back(aux);
         }
-        file.close();
+        initialfile.close();
 
         //Here we erase the last element of the vector, repeated due to the reading algorithm
-        cars.pop_back();
+        initialcars.pop_back();
 
-        int lane_number = stoi(cars[0]);
-        int locations = stoi(cars[1]);
+        int lane_number = stoi(initialcars[0]);
+        int locations = stoi(initialcars[1]);
 
-        cars.erase(cars.begin());
-        cars.erase(cars.begin());
+        initialcars.erase(initialcars.begin());
+        initialcars.erase(initialcars.begin());
 
         string init_parking_mat[lane_number][locations];
 
         for (int i = 0; i < lane_number; i++) {
           for (int j = 0; j < locations; j++) {
-            init_parking_mat[i][j] = cars[0];
-            cars.erase(cars.begin());
+            init_parking_mat[i][j] = initialcars[0];
+            initialcars.erase(initialcars.begin());
           }
         }
 
+        //First check print
         for (int i = 0; i < lane_number; i++) {
           for (int j = 0; j < locations; j++) {
             cout << init_parking_mat[i][j] << " ";
+          }
+          cout << endl;
+        }
+
+        //Here, we get the data from the goal file
+
+        vector<string> goalcars;
+
+        ifstream goalfile(argv[2]);
+        while (!goalfile.eof()) {
+                goalfile >> aux;
+                goalcars.push_back(aux);
+        }
+        goalfile.close();
+
+        //Here we erase the last element of the vector, repeated due to the reading algorithm
+        goalcars.pop_back();
+
+        lane_number = stoi(goalcars[0]);
+        locations = stoi(goalcars[1]);
+
+        goalcars.erase(goalcars.begin());
+        goalcars.erase(goalcars.begin());
+
+        string goal_parking_mat[lane_number][locations];
+
+        for (int i = 0; i < lane_number; i++) {
+          for (int j = 0; j < locations; j++) {
+            goal_parking_mat[i][j] = goalcars[0];
+            goalcars.erase(goalcars.begin());
+          }
+        }
+
+        //Second check print
+        cout << endl;
+        for (int i = 0; i < lane_number; i++) {
+          for (int j = 0; j < locations; j++) {
+            cout << goal_parking_mat[i][j] << " ";
           }
           cout << endl;
         }
