81073b344de26437f71b3dc7e5249c12cc9b4457
fernando2393 <garciasanzfernando2393@gmail.com>
2017-12-17 17:50:50 +0100

    Changes

--- Practice 2/AstarParking.cpp
+++ Practice 2/AstarParking.cpp
@@ -5,51 +5,76 @@
 
 using namespace std;
 
+/*
+   Operations:
+   Move forward in the same line: 1
+   Move backward in the same line: 2
+   Move to the beginning of another line: 3
+   Move to the end of another line: 4
+ */
+
 int heuristic(vector <string> init, vector <string> goal, int lane_number, int locations, string car){
-  //Here we introduce the initial and goal position and calculate the heuristic (minimum cost from init to goal)
-  int h;
-  int init_lane;
-  int init_column;
-  int goal_lane;
-  int goal_column;
+        //Here we introduce the initial and goal position and calculate the heuristic (minimum cost from init to goal)
+        int h;
+        int init_lane;
+        int init_column;
+        int goal_lane;
+        int goal_column;
 
-  for (int i = 0; i < lane_number; i++) {
-    for (int j = 0; j < locations; j++) {
-      if (init[i*locations+j]==car) {
-        init_lane=i;
-        init_column=j;
-        break;
-      }
-    }
-  }
-  for (int i = 0; i < lane_number; i++) {
-    for (int j = 0; j < locations; j++) {
-      if (goal[i*locations+j]==car) {
-        goal_lane=i;
-        goal_column=j;
-        break;
-      }
-    }
-  }
+        for (int i = 0; i < lane_number; i++) {
+                for (int j = 0; j < locations; j++) {
+                        if (init[i*locations+j]==car) {
+                                init_lane=i;
+                                init_column=j;
+                                break;
+                        }
+                }
+        }
+        for (int i = 0; i < lane_number; i++) {
+                for (int j = 0; j < locations; j++) {
+                        if (goal[i*locations+j]==car) {
+                                goal_lane=i;
+                                goal_column=j;
+                                break;
+                        }
+                }
+        }
 
-  h = abs(init_lane-goal_lane)+abs(init_column-goal_column);
-  if(h>0){
-    cout << "Initial lane: "<< init_lane << endl;
-    cout << "Initial column: "<< init_column << endl;
-    cout << "Goal lane: "<< goal_lane << endl;
-    cout << "Goal column: "<< goal_column << endl;
-    cout << endl;
-  }
-  return h;
+        h = abs(init_lane-goal_lane)+abs(init_column-goal_column);
+        return h;
 }
 
-void astar_search(){
+void astar_search(vector <string> init_parking_mat, vector <string> goal_parking_mat, int lane_number, int locations){
 
+        int totalheuristic = 0;
+        int heuristic_mat[lane_number][locations];
+
+
+        //Here we start the search selecting by ascendant heuristic order
+        for(int z = 1; z<(lane_number-1)+(locations-1); z++) {
+                for (int x = 0; x < lane_number; x++) {
+                        cout << endl;
+                        for (int y = 0; y < locations; y++) {
+                                //Checks the heuristic of each car from initial to goal position -> heuristic of the whole parking
+                                for (int i = 0; i < lane_number; i++) {
+                                        for (int j = 0; j < locations; j++) {
+                                                //Here we update the total heuristic for this iteration
+                                                totalheuristic += heuristic(init_parking_mat, goal_parking_mat, lane_number, locations, init_parking_mat[i*locations+j]);
+                                                //Map of heuristics creation for each car
+                                                heuristic_mat[i][j] = heuristic(init_parking_mat, goal_parking_mat, lane_number, locations, init_parking_mat[i*locations+j]);
+                                        }
+                                }
+                                cout << heuristic_mat[x][y];
+                                if(heuristic_mat[x][y]==z) {
+
+                                }
+                                totalheuristic = 0;
+                        }
+                }
+        }
 }
 
 int main(int argc, char const *argv[]) {
-
-        int totalheuristic = 0;
 
         vector<string> initialcars;
         string aux;
@@ -74,18 +99,18 @@ int main(int argc, char const *argv[]) {
         vector <string> init_parking_mat;
 
         for (int i = 0; i < lane_number; i++) {
-          for (int j = 0; j < locations; j++) {
-            init_parking_mat.push_back(initialcars[0]);
-            initialcars.erase(initialcars.begin());
-          }
+                for (int j = 0; j < locations; j++) {
+                        init_parking_mat.push_back(initialcars[0]);
+                        initialcars.erase(initialcars.begin());
+                }
         }
 
         //First check print
         for (int i = 0; i < lane_number; i++) {
-          for (int j = 0; j < locations; j++) {
-            cout << init_parking_mat[i*locations+j] << " ";
-          }
-          cout << endl;
+                for (int j = 0; j < locations; j++) {
+                        cout << init_parking_mat[i*locations+j] << " ";
+                }
+                cout << endl;
         }
 
         //Here, we get the data from the goal file
@@ -111,29 +136,22 @@ int main(int argc, char const *argv[]) {
         vector<string> goal_parking_mat;
 
         for (int i = 0; i < lane_number; i++) {
-          for (int j = 0; j < locations; j++) {
-            goal_parking_mat.push_back(goalcars[0]);
-            goalcars.erase(goalcars.begin());
-          }
+                for (int j = 0; j < locations; j++) {
+                        goal_parking_mat.push_back(goalcars[0]);
+                        goalcars.erase(goalcars.begin());
+                }
         }
 
         //Second check print
         cout << endl;
         for (int i = 0; i < lane_number; i++) {
-          for (int j = 0; j < locations; j++) {
-            cout << goal_parking_mat[i*locations+j] << " ";
-          }
-          cout << endl;
+                for (int j = 0; j < locations; j++) {
+                        cout << goal_parking_mat[i*locations+j] << " ";
+                }
+                cout << endl;
         }
 
-        for (int i = 0; i < lane_number; i++) {
-          for (int j = 0; j < locations; j++) {
-            totalheuristic = heuristic(init_parking_mat, goal_parking_mat, lane_number, locations, init_parking_mat[i*locations+j]) + totalheuristic;
-          }
-        }
-
-        cout << endl;
-        cout << "The total heuristic is: " << totalheuristic << endl;
+        astar_search( init_parking_mat, goal_parking_mat, lane_number, locations);
 
         return 0;
 }
